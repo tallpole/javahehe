38c8ed1938cb5ceb7df34b7dd8f60d98520c65b4
hanghank <suhang0410@gmail.com>
2017-10-11 14:27:37 -0700

    minor modification

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("I am adding some code to my project!!!");
 	}
 
 }
